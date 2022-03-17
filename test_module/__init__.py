cf0e48431bf3d3e5a33511a7b30740885dadfcc9
Tom Svilans <tsvi@kadk.dk>
2022-03-17 14:11:49 +0100

    added test_module/__init__.py

---
+++ test_module/__init__.py
@@ -0,0 +1,12 @@
+import math
+
+class Shape():
+    def __init__(self):
+        self.color = None
+        self.name = "Shape"
+
+    def change_color(self, color):
+        self.color = color
+
+if __name__=="__main__":
+    pass
