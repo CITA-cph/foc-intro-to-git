3215e6e7e689f9f9c03625f4614b6fd55adb1dac
Tom Svilans <email@example.com>
2022-03-17 14:38:57 +0100

    added TomSvilans class

---
+++ test_module/TomSvilans.py
@@ -0,0 +1,12 @@
+class TomSvilans():
+    def __init__(self):
+        self.height = 1850
+        self.weight = 50
+        self.age = 200
+
+    def function(self):
+        return 1 + 1
+
+if __name__=="__main__":
+    print("hello")
+    pass
